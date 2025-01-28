f2ba3f436fa50994cff72fddb08840253465d43d
RTRTRTRTRT3 <raniashalkar@gmail.com>
2025-01-29 00:06:05 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'Welcome_Screen.dart'; // Убедитесь, что путь к файлу указан правильно.
 import 'Home_Screen.dart'; // Импортируйте HomeScreen
 import 'Details_Screen.dart'; // Импортируйте DetailsScreen
-import 'Login_Page.dart'; // Импортируйте LoginPage
+import 'Login_Page.dart'; // Убедитесь, что путь к файлу указан правильно.
 
 void main() {
   runApp(MyApp());
@@ -17,10 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomeScreen(), // Укажите стартовый экран.
+      home: WelcomeScreen(), // Укажите WelcomeScreen как стартовый экран.
       routes: {
+        '/login': (context) => LoginPage(), // Убедитесь, что здесь используется LoginPage
         '/home': (context) => HomeScreen(), // Маршрут к HomeScreen
-        '/login': (context) => LoginPage(), // Маршрут к LoginPage
       },
     );
   }
